d31f7b5dbb4ee8c84606f8416301389f41af9f91
Troy Straszheim <straszheim@willowgarage.com>
2011-02-06 10:09:58 -0800

    more

--- codingstds/82.cpp
+++ codingstds/82.cpp
@@ -14,20 +14,37 @@ void show(std::vector<int>& v)
 int main() {
 
   std::vector<int> vi(10);
-  vi[1] = 7;
-  vi[3] = 7;
+  vi[0] = 9;
+  vi[1] = 8;
+  vi[2] = 7;
+  vi[3] = 6;
+  vi[4] = 5;
+  vi[5] = 4;
+  vi[6] = 3;
+  vi[7] = 2;
+  vi[8] = 1;
 
+  std::cout << "initial vector...\n";
   show(vi);
-  vi.resize(8);
 
+
+  std::cout << "resize 8, swap with itself...\n";
+  vi.resize(8);
   std::vector<int>(vi).swap(vi);
   show(vi);
 
 
-  std::remove(vi.begin(), vi.end(), 7);
+  std::cout << "remove 7...\n";
+  std::vector<int>::iterator iter = std::remove(vi.begin(), vi.end(), 7);
+  std::cout << "iter at " << std::distance(vi.begin(), iter) << "\n";
   show(vi);
 
-  vi.erase(std::remove(vi.begin(), vi.end(), 7), vi.end());
+  std::cout << "erase (w/ iter from remove)...\n";
+  vi.erase(iter, vi.end());
+  show(vi);
+
+  std::cout << "resize doesn't change capacity\n";
+  vi.resize(0);
   show(vi);
 
   std::cout << "done\n";
