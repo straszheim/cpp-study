6aef65c53789943ba775fa13adb35670d288315f
Troy Straszheim <troy@resophonic.com>
2016-12-26 23:58:08 -0800

    meH'

---
+++ mathy.cpp
@@ -0,0 +1,16 @@
+#include <Eigen/Geometry>
+#include <iostream>
+#include <sophus/se2.hpp>
+#include "study.hpp"
+#include <stddef.h>
+
+template <typename T, int Dim>
+struct Mat {
+  T m[Dim];
+};
+
+int main()
+{
+  Mat<float, 3> v3;
+
+}
